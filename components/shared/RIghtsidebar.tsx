95c9181c2beb320a5782566749efd54607328a3a
Ali-dev <alin89328@gmail.com>
2023-08-25 19:53:25 +0500

    feat: wip right-sidebar

--- components/shared/RIghtsidebar.tsx
+++ components/shared/RIghtsidebar.tsx
@@ -1,5 +1,16 @@
-function Rightsidebar() {
-	return <h1>Rightsidebar</h1>
+function RightSidebar() {
+	return (
+		<section className='custom-scrollbar rightsidebar'>
+			<div className='flex flex-1 flex-col justify-start'>
+				<h3 className='text-heading4-medium text-light-1'>
+					Suggested Communities
+				</h3>
+			</div>
+			<div className='flex flex-1 flex-col justify-start'>
+				<h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
+			</div>
+		</section>
+	)
 }
 
-export default Rightsidebar
+export default RightSidebar
