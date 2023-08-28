0a0254455132037590f1d5530c2d0326298702f9
Ali-dev <alin89328@gmail.com>
2023-08-28 17:55:32 +0500

    feat: fetchuser action and community model

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import User from '../models/user.model'
+import Community from '../models/community.model'
 import {connectToDB} from '../mongoose'
 import {revalidatePath} from 'next/cache'
 
@@ -41,5 +42,18 @@ export async function updateUser({
 		}
 	} catch (error: any) {
 		throw new Error(`Failed to create/update user: ${error.message}`)
+	}
+}
+
+export async function fetchUser(userId: string) {
+	try {
+		connectToDB()
+
+		return await User.findOne({id: userId}).populate({
+			path: 'communities',
+			model: Community,
+		})
+	} catch (error: any) {
+		throw new Error(`Failed to fetch user: ${error.message}`)
 	}
 }
