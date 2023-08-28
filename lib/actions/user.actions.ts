1fd799bdd7deaad6d272e9ca285c406f59500d95
Ali-dev <alin89328@gmail.com>
2023-08-28 19:27:23 +0500

    feat: created landing page for create thread and thread actions to create thread

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -49,10 +49,11 @@ export async function fetchUser(userId: string) {
 	try {
 		connectToDB()
 
-		return await User.findOne({id: userId}).populate({
-			path: 'communities',
-			model: Community,
-		})
+		return await User.findOne({id: userId})
+		// .populate({
+		// 	path: 'communities',
+		// 	model: Community,
+		// })
 	} catch (error: any) {
 		throw new Error(`Failed to fetch user: ${error.message}`)
 	}
