e968bbd6bd1501dfb7c56c3e7a41890e46cfb7e0
Ali-dev <alin89328@gmail.com>
2023-08-29 15:29:26 +0500

    feat: created thread card and display it on home page

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -26,7 +26,7 @@ export async function createThread({text, author, communityId, path}: Params) {
 		const createdThread = await Thread.create({
 			text,
 			author,
-			community: communityIdObject,
+			community: null,
 		})
 
 		await User.findByIdAndUpdate(author, {
@@ -42,5 +42,42 @@ export async function createThread({text, author, communityId, path}: Params) {
 		revalidatePath(path)
 	} catch (error: any) {
 		throw new Error(`Failed to create thread: ${error.message}`)
+	}
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+	try {
+		connectToDB()
+
+		const skipAmount = (pageNumber - 1) * pageSize
+
+		const postsQuery = Thread.find({parentId: {$in: [null, undefined]}})
+			.sort({createdAt: 'desc'})
+			.skip(skipAmount)
+			.limit(pageSize)
+			.populate({
+				path: 'author',
+				model: User,
+			})
+			.populate({
+				path: 'children',
+				populate: {
+					path: 'author',
+					model: User,
+					select: '_id name parentId image',
+				},
+			})
+
+		const totalPostsCount = await Thread.countDocuments({
+			parentId: {$in: [null, undefined]},
+		})
+
+		const posts = await postsQuery.exec()
+
+		const isNext = totalPostsCount > skipAmount + posts.length
+
+		return {posts, isNext}
+	} catch (error: any) {
+		throw new Error(`Failed to fetch posts: ${error.message}`)
 	}
 }
