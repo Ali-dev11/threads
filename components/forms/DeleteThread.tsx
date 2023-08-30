d2f8401ea509e3900af196edb93610ec581cb3f4
Ali-dev <alin89328@gmail.com>
2023-08-30 19:26:36 +0500

    refactor: fix _id null error

--- components/forms/DeleteThread.tsx
+++ components/forms/DeleteThread.tsx
@@ -28,7 +28,7 @@ function DeleteThread({
 	return (
 		<Image
 			src='/assets/delete.svg'
-			alt='delte'
+			alt='delete'
 			width={18}
 			height={18}
 			className='cursor-pointer object-contain'
