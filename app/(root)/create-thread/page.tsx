0c05a880c23164b19814b61bac6ecc5dcb14feae
Ali-dev <alin89328@gmail.com>
2023-08-30 12:05:10 +0500

    feat: created a page for thread by id

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -14,7 +14,7 @@ async function Page() {
 	return (
 		<>
 			<h1 className='head-text'>Create Thread</h1>
-			<PostThread userId={userInfo._id} />
+			<PostThread userId={JSON.stringify(userInfo._id)} />
 		</>
 	)
 }
