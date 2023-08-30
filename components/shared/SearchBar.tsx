f29e62d9cfefc4923ac97f9c99affb3fea7865b9
Ali-dev <alin89328@gmail.com>
2023-08-30 18:57:36 +0500

    feat: added search and pagination in root and search page

--- components/shared/SearchBar.tsx
+++ components/shared/SearchBar.tsx
@@ -40,7 +40,7 @@ function Searchbar({routeType}: Props) {
 				value={search}
 				onChange={(e) => setSearch(e.target.value)}
 				placeholder={`${
-					routeType !== '/search' ? 'Search communities' : 'Search creators'
+					routeType !== 'search' ? 'Search communities' : 'Search creators'
 				}`}
 				className='no-focus searchbar_input'
 			/>
